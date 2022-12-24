5473521f406b3a4a64410ee21477a927fbd0e462
JohnC7777 <john.citrowske@gmail.com>
2022-12-23 19:17:11 -0600

    Initial Commit

---
+++ Cognitive Bias App V2/Cognitive_Bias_App_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cognitive_Bias_App_V2App.swift
+//  Cognitive Bias App V2
+//
+//  Created by user230876 on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Cognitive_Bias_App_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
